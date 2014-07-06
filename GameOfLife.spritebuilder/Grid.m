04b8acfa4b5e6610051bb6cf7565c5d46264c32d
VuochNin <NewOwner@Jesse-Hinshaws-MacBook-Pro.local>
2014-07-06 11:57:03 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -59,9 +59,7 @@ static const int GRID_COLUMNS = 10;
                 //this is shorthand to access an array inside an array
                 _gridArray[i][j] = creature;
                 
-                //make creatures visible to test this method, remove this once
-                //we know we have filled the grid properly
-                //creature.isAlive = YES;
+                
                 
                 x+=_cellWidth;
             }
