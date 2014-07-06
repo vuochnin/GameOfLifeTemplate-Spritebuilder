45262f86faadaec014f3cfe04c1eed270ebba731
VuochNin <NewOwner@Jesse-Hinshaws-MacBook-Pro.local>
2014-07-05 21:18:56 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -13,7 +13,9 @@
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
 
-
+- (void) evolveStep;
+- (void) countNeighbors;
+- (void) updateCreatures;
 
 
 @end
